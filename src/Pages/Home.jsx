4738f0d96cf10370db8b33c28c4b40aa31c9970f
mdmomenulislam1 <mdmomenulislam1@gmail.com>
2023-10-25 23:05:22 +0600

    Login Issues solved

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,9 +2,18 @@ import { Link, useLoaderData } from "react-router-dom";
 import Banner from "../Component/Banner";
 import React from 'react'
 import ServiceCard from "../Component/ServiceCard";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Home = () => {
     const services = useLoaderData();
+
+    AOS.init({
+        duration: 800,  
+        offset: 200,    
+        easing: 'ease', 
+    });
+
 
     const containerStyle = {
         backgroundImage: `url('https://i.ibb.co/dgXcv7D/istockphoto-1399000012-612x612.webp'), linear-gradient(180deg, rgba(0, 0, 0, 0.8), rgba(9, 9, 9, 0.8))`,
@@ -16,22 +25,22 @@ const Home = () => {
         padding: '20px',
         borderRadius: '5px',
     };
-    
+
 
 
 
     return (
         <div className="px-8 md:px-10 lg:px-14 mx-auto">
             <Banner></Banner>
-            <h2 className="text-5xl font-bold text-black text-center my-10">Services</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 px-5 lg:px-0 lg:grid-cols-4 gap-3 md:gap-5 lg:gap-8 lg:my-10">
+            <h2 data-aos="fade-down" className="text-5xl font-bold text-black text-center my-10">Services</h2>
+            <div data-aos="fade-up" className="grid grid-cols-1 md:grid-cols-2 px-5 lg:px-0 lg:grid-cols-4 gap-3 md:gap-5 lg:gap-8 lg:my-10">
                 {
                     services?.map(service => <ServiceCard key={service.id} service={service}></ServiceCard>)
                 }
             </div>
 
-            <h2 className="text-4xl font-bold text-black text-center">Meet Up </h2>
-            <div
+            <h2 data-aos="fade-left" className="text-4xl font-bold text-black text-center">Meet Up </h2>
+            <div data-aos="fade-right"
                 style={containerStyle}
                 className="mt-16 p-16 rounded-xl">
                 <div className="text-center">
@@ -43,7 +52,7 @@ const Home = () => {
                 </div>
 
                 {/* <!-- Person Container  --> */}
-                <div className="mt-10 p-10 flex justify-around items-center gap-5 flex-col md:flex-row">
+                <div  className="mt-10 p-10 flex justify-around items-center gap-5 flex-col md:flex-row">
 
 
                     {/* <!-- First Person  --> */}
@@ -104,8 +113,8 @@ const Home = () => {
             </div>
 
 
-            <h2 className="text-4xl font-bold text-black text-center my-5">Offers</h2>
-            <div>
+            <h2 data-aos="zoom-in" className="text-4xl font-bold text-black text-center my-5">Offers</h2>
+            <div data-aos="zoom-out">
                 <section className="bg-gray-100 py-16 rounded-lg my-10">
                     <div className="container mx-auto text-center px-10">
                         <h2 className="text-3xl font-semibold text-gray-800 mb-6">Special Offers</h2>
@@ -114,21 +123,21 @@ const Home = () => {
                             <div className="bg-white p-6 rounded-lg shadow-lg">
                                 <h3 className="text-xl font-semibold text-gray-800 mb-4">20% Off on First Purchase</h3>
                                 <p className="text-gray-600 mb-4">Use code: FIRST20 during checkout to get 20% off on your first purchase.</p>
-                                
+
                             </div>
 
                             {/* Offer Card 2 */}
                             <div className="bg-white p-6 rounded-lg shadow-lg">
                                 <h3 className="text-xl font-semibold text-gray-800 mb-4">Free Shipping</h3>
                                 <p className="text-gray-600 mb-4">Enjoy free shipping on orders over $50. No promo code needed.</p>
-                                
+
                             </div>
 
                             {/* Offer Card 3 */}
                             <div className="bg-white p-6 rounded-lg shadow-lg">
                                 <h3 className="text-xl font-semibold text-gray-800 mb-4">Limited-Time Sale</h3>
                                 <p className="text-gray-600 mb-4">Shop our limited-time sale items and save big on your favorite products.</p>
-                                
+
                             </div>
                         </div>
                     </div>
